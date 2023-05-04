f4084a631a2e5701fb452c9df9818309f500a904
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-05-04 17:38:02 +0300

    Added method of creating dialog element.

--- currenciesWidget.js
+++ currenciesWidget.js
@@ -9,11 +9,29 @@ const BASE = 'UAH';
 
 const getResponse = (response) => {
   const frag = document.createDocumentFragment();
+  
   for (let i in response) {
     frag.appendChild(document.createTextNode(i + '#' + response[i]));
     frag.appendChild(document.createElement('br'));
   }
-  document.querySelector('body').appendChild(frag);
+  const div = document.createElement('div');
+  div.setAttribute('color', 'yellow');
+  div.appendChild(frag);
+  document.querySelector('body').appendChild(div);
+};
+
+const createDialog = (placeholder = "Please, enter your API-key") => {
+  const fragment = document.createDocumentFragment();
+  const textarea = fragment
+    .appendChild(document.createElement("dialog"))
+    .appendChild(document.createElement("section"))
+    .appendChild(document.createElement("textarea"));
+  textarea.placeholder = placeholder;
+  
+  document.body.appendChild(fragment);
+  
+  let dialog = document.querySelector("dialog");
+  dialog.setAttribute("open", "true");
 };
 
 fetch(`https://min-api.cryptocompare.com/data/price?fsym=${BASE}&tsyms=USD,JPY,EUR&api_key={${savedKey}}`, {
