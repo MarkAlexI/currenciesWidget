be87857b071b5012885876b216905af6cffa7d8b
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-05-06 19:58:11 +0300

    Refactoring of project.

--- currenciesWidget.js
+++ currenciesWidget.js
@@ -1,51 +1,102 @@
 'use strict';
 
+const BASE = 'UAH';
+
 const enterToDialog = document.body.appendChild(document.createElement('div'));
 
 enterToDialog.innerHTML = `
 <style>
   #currency_widget_dialog {
     background-color: lime;
+    z-index: 10;
+    margin-top: 10 px;
+    border-radius: 1rem;
+  }
+  #currency_widget_dialog::backdrop {
+    background-color: hsl(250, 100%, 50%, 0.25);
   }
 </style>
-<dialog id="currency_widget_dialog">
-  <form>
-    <input type="text" placeholder="@#$" />
-    <button formmethod="dialog" type="submit">Cancel</button>
-    <button type="submit">Submit</button>
+<dialog
+  id="currency_widget_dialog"
+>
+  <form onsubmit="submitForm(event)">
+    <input
+      id="currency_widget_input"
+      type="text"
+      placeholder="Print your API-key"
+    />
+    <button
+      id="currency_widget_left_btn"
+      formmethod="dialog"
+      type="submit"
+    >Cancel</button>
+    <button
+      id="currency_widget_right_btn"
+      type="submit"
+    >Submit</button>
   </form>
 </dialog>
 `;
 
 const dialog = document.getElementById("currency_widget_dialog");
+const input = document.getElementById("currency_widget_input");
+const leftBtn = document.getElementById("currency_widget_left_btn");
+const rightBtn = document.getElementById("currency_widget_right_btn");
 
-const savedKey = localStorage.getItem('key') ||
-  prompt('Print your API-key', '');
+const openDialog = () => !dialog.open && dialog.showModal();
 
-!!savedKey && localStorage.setItem('key', savedKey);
+const closeDialog = () => dialog.close(); openDialog();
 
-const BASE = 'UAH';
+let savedKey = localStorage.getItem('key');
+
+const saveLocal = () => localStorage.setItem('key', savedKey);
+
+const takeFromInput = () => {
+  if (input.value) {
+    savedKey = input.value;
+    input.value = '';
+    saveLocal();
+    closeDialog();
+  }
+};
+
+leftBtn.addEventListener('click', closeDialog);
+rightBtn.addEventListener('click', takeFromInput);
 
 const getResponse = (response) => {
   const frag = document.createDocumentFragment();
-  
+
   for (let i in response) {
     frag.appendChild(document.createTextNode(i + '#' + response[i]));
     frag.appendChild(document.createElement('br'));
   }
   const div = document.createElement('div');
-  div.setAttribute('color', 'yellow');
   div.appendChild(frag);
   document.querySelector('body').appendChild(div);
 };
 
-fetch(`https://min-api.cryptocompare.com/data/price?fsym=${BASE}&tsyms=USD,JPY,EUR&api_key={${savedKey}}`, {
-    credentials: 'omit'
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then(getResponse)
-  .catch((error) => {
-    console.log(error);
-  });
+const getCources = () => {
+  fetch(`https://min-api.cryptocompare.com/data/price?fsym=${BASE}&tsyms=USD,JPY,EUR&api_key={${savedKey}}`, {
+      credentials: 'omit'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then(getResponse)
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+if (savedKey?.length > 0) {
+  getCources();
+} else {
+  openDialog();
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  window.history.back();
+  !!input.value && getCources();
+  return false;
+}
