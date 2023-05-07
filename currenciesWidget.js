a0412303b6b1af51f249c7ce1b03c0adb7637189
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-05-07 10:13:27 +0300

    Added styles to widget.

--- currenciesWidget.js
+++ currenciesWidget.js
@@ -15,6 +15,23 @@ enterToDialog.innerHTML = `
   #currency_widget_dialog::backdrop {
     background-color: hsl(250, 100%, 50%, 0.25);
   }
+  #currency_widget_field {
+    position: fixed;
+    bottom: 5rem;
+    right: 2rem;
+    z-index: 10;
+    border: 1px solid black;
+  }
+  #currency_widget_field table {
+    width: 9rem;
+    border-collapse: collapse;
+    border: 1 px solid black;
+    text-align: center;
+    vertical-align: middle;
+  }
+  #currency_widget_data tr:nth-child(odd) {
+    background-color: linen;
+  }
 </style>
 <dialog
   id="currency_widget_dialog"
@@ -36,16 +53,25 @@ enterToDialog.innerHTML = `
     >Submit</button>
   </form>
 </dialog>
+<div id="currency_widget_field">
+  <table>
+    <tbody id="currency_widget_data">
+      
+    </tbody>
+  </table>
+</div>
 `;
 
 const dialog = document.getElementById("currency_widget_dialog");
 const input = document.getElementById("currency_widget_input");
 const leftBtn = document.getElementById("currency_widget_left_btn");
 const rightBtn = document.getElementById("currency_widget_right_btn");
+const field = document.getElementById("currency_widget_field");
+const data = document.getElementById("currency_widget_data");
 
 const openDialog = () => !dialog.open && dialog.showModal();
 
-const closeDialog = () => dialog.close(); openDialog();
+const closeDialog = () => dialog.close();
 
 let savedKey = localStorage.getItem('key');
 
@@ -64,15 +90,20 @@ leftBtn.addEventListener('click', closeDialog);
 rightBtn.addEventListener('click', takeFromInput);
 
 const getResponse = (response) => {
-  const frag = document.createDocumentFragment();
+  let content = '';
 
-  for (let i in response) {
-    frag.appendChild(document.createTextNode(i + '#' + response[i]));
-    frag.appendChild(document.createElement('br'));
-  }
-  const div = document.createElement('div');
-  div.appendChild(frag);
-  document.querySelector('body').appendChild(div);
+  Object
+    .keys(response)
+    .map(currencyCode => {
+      content += `
+        <tr>
+          <td>${currencyCode}</td>
+          <td>${(1/response[currencyCode]).toFixed(3)}</td>
+        </tr>
+      `
+    });
+
+  data.innerHTML = content;
 };
 
 const getCources = () => {
